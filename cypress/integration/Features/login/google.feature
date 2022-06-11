a30a0bff274ee09e06bc49e69b632086cc45d9c6
Ariol Keubeng <ariol.keubeng@gmail.com>
2022-06-11 10:01:23 +0200

    one feature successfully tested

---
+++ cypress/integration/Features/login/google.feature
@@ -0,0 +1,13 @@
+Feature: Login
+
+    I want to open a search engine
+
+    @focus
+    Scenario: using google
+        Given I open Google page
+        Then I see "Google" in the title
+
+
+
+
+
---
+++ cypress/integration/Step definitions/google.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+import { Given } from "cypress-cucumber-preprocessor/steps";
+
+const url = "https://google.com";
+Given("I open Google page", () => {
+  cy.visit(url);
+});
+
