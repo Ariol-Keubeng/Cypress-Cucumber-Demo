86850e3681d60dcaf661e57e68cdf757ae1d7723
Ariol Keubeng <ariol.keubeng@gmail.com>
2022-06-11 16:06:36 +0200

    details and home page enriched

--- app-example/src/app/pages/details/details.component.ts
+++ app-example/src/app/pages/details/details.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
-
-  constructor() { }
+  item: any 
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(history.state);
+    this.item = history.state;
   }
-
 }
--- app-example/src/app/pages/home/home.component.ts
+++ app-example/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() {}
+  products = [
+    { title: 'Galaxy S20 Neu', description: '', price: 20 },
+    { title: 'Elecktor Fahrad', description: '', price: 420 },
+    { title: 'Zahnbürste Neu', description: '', price: 30 },
+    { title: 'Kinderschuhe', description: '', price: 13 },
+    { title: 'Raspberry 4', description: '', price: 25 },
+    { title: 'Opel Corsa MY200', description: '', price: 20 },
+  ];
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
